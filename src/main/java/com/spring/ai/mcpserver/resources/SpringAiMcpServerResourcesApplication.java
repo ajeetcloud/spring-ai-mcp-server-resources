23e396b325751ade2ae79dd6dd20e73383a171a4
Ajeet Singh <ajeet.cloud@gmail.com>
2025-11-14 17:36:07 +0530

    Initial commit

---
+++ src/main/java/com/spring/ai/mcpserver/resources/SpringAiMcpServerResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ai.mcpserver.resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMcpServerResourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiMcpServerResourcesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/ai/mcpserver/resources/SpringAiMcpServerResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai.mcpserver.resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMcpServerResourcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
